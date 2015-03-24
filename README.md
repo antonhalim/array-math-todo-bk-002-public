28f2849e9813499d7d159af9f630a671d2a9c154
Katie Hoffman <ktahoffman@gmail.com>
2015-03-24 11:14:26 -0400

    set up structure

---
+++ README.md
@@ -0,0 +1,75 @@
+---
+tags: logic, cs, monkey patching, todo, custom methods
+languages: ruby
+resources: 1
+---
+
+# Array Math
+
+![math operators](https://s3-us-west-2.amazonaws.com/web-dev-readme-photos/cs/math.jpg)
+
+## Objective
+
+You will be building out Ruby's array `#*` and `#+` methods. Take a look at them below.
+
+## `#+`
+
+The `+` method adds one array to another array:
+
+```ruby
+[ 1, 2, 3 ] + [ 4, 5 ]
+#=> [ 1, 2, 3, 4, 5 ]
+
+[ "a", "b", "c" ] + [ "d", "e", "f" ]
+#=> [ "a", "b", "c", "d", "e", "f" ]
+```
+Take a look at the Ruby docs on `#+` (http://ruby-doc.org/core-2.2.0/Array.html#method-i-2B). You'll be calling your version of this method `#plus`.
+
+## `#*`
+
+The simple version of the `*` method duplicates the elements in the array by the number that follows the asterisk.
+
+```ruby
+[ "a", "b"] * 2
+#=> ["a", "b", "a", "b"]
+
+[ 1, 2, 3 ] * 3
+#=> [ 1, 2, 3, 1, 2, 3, 1, 2, 3 ]
+```
+
+However, if a string follows the asterisk, then the array is joined on that string.
+
+```ruby
+[ 1, 2, 3 ] * ","
+#=> "1,2,3"
+```
+
+Take a look at the Ruby docs on `#*` [here](http://ruby-doc.org/core-2.2.0/Array.html#method-i-2A). You'll be calling your version of this method `#asterisk`.
+
+## `#&`
+
+The `&` method returns an array containing every element that the two arrays have in common. This returned array is unique (has no duplicate characters) and the order of the first array is preserved.
+
+```ruby
+[ 1, 1, 3, 5 ] & [ 1, 2, 3 ]
+#=> [ 1, 3 ]
+
+[ 'a', 'b', 'b', 'z' ] & [ 'a', 'b', 'c' ]
+#=> [ 'a', 'b' ]
+
+
+[ 'a', 'm', 's', 'z' ] & [ 'a', 'b', 'c' ]
+#=> [ 'a', 'b' ]
+```
+
+Take a look at the Ruby docs on `#&` [here](http://ruby-doc.org/core-2.2.0/Array.html#method-i-26). You'll be calling your version of this method `#ampersand`.
+
+## Notes
+
+Do not call on Ruby's `#*`, `#+`, and ``#&` methods to accomplish this.
+
+## Resources
+
+* [Plus Sign Method](http://ruby-doc.org/core-2.2.0/Array.html#method-i-2B)
+* [Asterisk Method](http://ruby-doc.org/core-2.2.0/Array.html#method-i-2A)
+* [Ampersand Method](http://ruby-doc.org/core-2.2.0/Array.html#method-i-26)
---
+++ spec/array_math_spec.rb
@@ -0,0 +1,106 @@
+describe "Array Math" do
+
+  let(:a) { [ 4, 5, 6 ] }
+  let(:b) { [ 7, 8, 9 ] }
+
+  let(:evens) { [ 2, 4, 6 ] }
+  let(:odds) { [ 7, 9, 11 ] }
+
+  let(:animals)     { ["aardvark", "hedgehog", "wallaby"] }
+  let(:ingredients) { ["anise",    "honey",    "wasabi" ] }
+
+  describe "#plus" do
+    it "doesn't use Ruby's plus method" do
+      expect_any_instance_of(Array).to_not receive(:+)
+      a.plus(b)
+    end
+
+    it "doesn't change either array" do
+      a.plus(b)
+      expect(a).to eq(a)
+      expect(b).to eq(b)
+    end
+
+    it "adds two arrays together" do
+      expect(a.plus(b)).to eq([ 4, 5, 6, 7, 8, 9 ])
+      expect(b.plus(a)).to eq([ 7, 8, 9, 4, 5, 6 ])
+      expect(evens.plus(odds)).to eq([ 2, 4,  6, 7, 9, 11 ])
+      expect(odds.plus(evens)).to eq([ 7, 9, 11, 2, 4,  6 ])
+      result = ["aardvark", "hedgehog", "wallaby", "anise", "honey", "wasabi"]
+      expect(animals.plus(ingredients)).to eq(result)
+    end
+  end
+
+  describe "#asterisk" do
+    it "doesn't use Ruby's plus method" do
+      expect_any_instance_of(Array).to_not receive(:*)
+      a.asterisk(3)
+    end
+
+    it "doesn't change the array" do
+      a.asterisk(3)
+      expect(a).to eq(a)
+    end
+
+    context "Integer Argument" do
+      it "duplicates items in array 'arg' number of times" do
+        expect(a.asterisk(3)).to eq([4, 5, 6, 4, 5, 6, 4, 5, 6])
+        expect(b.asterisk(2)).to eq([7, 8, 9, 7, 8, 9])
+        result = ["aardvark", "hedgehog", "wallaby", "aardvark", "hedgehog", "wallaby"]
+        expect(animals.asterisk(2)).to eq(result)
+      end
+    end
+
+    context "String Argument" do
+      it "joins the array by the argument" do
+        expect(animals.asterisk(", ")).to eq("aardvark, hedgehog, wallaby")
+        expect(ingredients.asterisk("+")).to eq("anise+honey+wasabi")
+      end
+    end
+  end
+
+  describe "#ampersand" do
+    let(:first_prime)    { [  2,  3,  5,  7, 11, 13, 17, 19, 23, 29 ] }
+    let(:reversed_prime) { [ 29, 23, 19, 17, 13, 11,  7,  5,  3,  2 ] }
+    let(:first_odd)      { [  1,  3,  5,  7,  9, 11, 13, 15, 17, 19 ] }
+    let(:multiples_of_5) { [  5, 10, 15, 20, 25, 30, 35, 40, 45, 50 ] }
+
+    let(:alphabet) { ["a", "b", "c", "d", "e", "f", "g", "h", "i"] }
+    let(:vowels)   { ["u", "o", "i", "e", "a"] }
+    let(:love)     { ["l", "o", "v", "e"] }
+
+    it "doesn't use Ruby's ampersand method" do
+      expect_any_instance_of(Array).to_not receive(:&)
+      a.ampersand(b)
+    end
+
+    it "doesn't change either array" do
+      a.ampersand(b)
+      expect(a).to eq(a)
+      expect(b).to eq(b)
+    end
+
+    it "finds the common element and puts it in an array when there is only one" do    
+      expect(multiples_of_5.ampersand(first_prime)).to eq([5])
+      expect(alphabet.ampersand(love)).to eq(["e"])
+    end
+
+    it "returns array of shared elements in the order established by the first_odd
+      array when there are two common elements" do
+      expect(multiples_of_5.ampersand(first_odd)).to eq([5, 15])
+      expect(love.ampersand(vowels)).to eq(["o", "e"])
+    end
+
+    it "returns array of shared elements in the order established by the first_odd
+      array when there are more than two common elements" do
+      expect(alphabet.ampersand(vowels)).to eq(["a", "e", "i"])
+      expect(vowels.ampersand(alphabet)).to eq(["i", "e", "a"])
+      expect(first_prime.ampersand(first_odd)).to eq([3, 5, 7, 11, 13, 17, 19])
+      expect(reversed_prime.ampersand(first_odd)).to eq([19, 17, 13, 11, 7, 5, 3])
+    end
+
+    it "returns an empty array when there are no common elements" do
+      expect(alphabet.ampersand(first_prime)).to eq([])
+    end
+  end
+end
