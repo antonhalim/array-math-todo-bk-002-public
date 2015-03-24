fafb7994dc9b60a9f997bea61d187fa329d582f1
Katie Hoffman <ktahoffman@gmail.com>
2015-03-24 11:31:36 -0400

    pass tests

--- lib/array_math.rb
+++ lib/array_math.rb
@@ -1,1 +1,24 @@
-# your code goes here
+class Array
+  def plus(array)
+    result = [self]
+    result << array
+    result.flatten
+  end
+
+  def asterisk(arg)
+    return self.join(arg) if arg.class == String
+    result = []
+    arg.times { result << self }
+    result.flatten
+  end
+
+  def ampersand(array)
+    counts = (self + array).each_with_object({}) do |e, counts|
+      counts[e] ||= 0
+      counts[e] += 1
+    end
+    common = counts.group_by { |e,c| c > 1 }[true]
+    return [] if common.nil?
+    common.collect {|a| a[0]}
+  end
+end
