9cd137696a69e02eb179c99e7b158630a48243ae
axton21 <axton_n@yahoo.com>
2015-03-27 00:27:00 -0400

    done

--- lib/array_math.rb
+++ lib/array_math.rb
@@ -1,1 +1,25 @@
-# your code goes here
+require 'pry'
+class Array
+
+	def plus(array)
+		result = Array.new(self)
+		array.each{|x| result.push(x)}
+		result
+	end
+
+	def asterisk(num)
+		result = Array.new(self)
+		if num.class == Fixnum
+			 (1...num).step{|x| result << self}
+		# (num-1).times do result << self end
+			return result.flatten
+		else
+			return result.join(num)
+		end
+	end
+
+	def ampersand(array)
+		self.select{|x| array.include?(x)}
+	end
+
+end
